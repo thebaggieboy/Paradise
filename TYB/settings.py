2eb2c96485054f2ce1ccfaaceb1e5afc98483357
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-11-03 00:22:29 +0100

    preparing for render deployment

--- TYB/settings.py
+++ TYB/settings.py
@@ -148,7 +148,8 @@ AUTHENTICATION_BACKENDS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SITE_ID = 1
